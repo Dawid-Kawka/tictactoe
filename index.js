fc1918a64f22190f58437497af3a23613c118c2e
Dawid Kawka <d.kawka@op.pl>
2022-02-02 12:10:24 +0100

    add checkWinner and winningVariants

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ window.onload = function () {
 }
 
 class App {
+    winningVariants = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    currentPlayer = "X";
 
     init() {
         document.querySelectorAll(".cell").forEach(
@@ -15,6 +27,15 @@ class App {
 
     playerTurn(el) {
         if (el.innerHTML == "X" || el.innerHTML == "O") return;
+        el.innerHTML = this.currentPlayer;
+
+        this.currentPlayer = this.currentPlayer == "X" ? "O" : "X";
+
+        this.checkWinner();
+    }
+
+    checkWinner() {
+
     }
 }
 
