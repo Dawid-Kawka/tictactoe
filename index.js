628804469a0b298817d35ea6345fb74d7789ae30
Dawid Kawka <d.kawka@op.pl>
2022-02-02 12:02:53 +0100

    connect js with html cells

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ window.onload = function () {
 }
 
 class App {
-    init() {
 
+    init() {
+        document.querySelectorAll(".cell").forEach(
+            cell => cell.addEventListener("click", this.cellClick));
+    }
+    cellClick = (e) => {
+        //console.log(e.target)
+        this.playerTurn(e.target)
     }
 
+    playerTurn(el) {
+        if (el.innerHTML == "X" || el.innerHTML == "O") return;
+    }
 }
 
 const app = new App();
